4f9a7e498c701069c255f8b77d4a40b2739b4ffc
Jagan Pradhan <jaganp515@gmail.com>
2026-02-16 00:30:48 +0530

    fix

--- src/config/appConfig.jsx
+++ src/config/appConfig.jsx
@@ -1,11 +1,2 @@
-/*
-    Central place to manage API base URL
-    Automatically switches based on environment
-*/
-
-const isProduction = import.meta.env.PROD;
-
-export const serverEndpoint = isProduction
-    ? import.meta.env.VITE_SERVER_URL   // Production API
-    : "http://localhost:5001";          // Local backend
-
+export const serverEndpoint =
+  import.meta.env.VITE_SERVER_URL;
